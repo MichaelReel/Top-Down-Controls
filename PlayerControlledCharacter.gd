c5124e433ff158acb97522d1e09472b878802896
Michael Reel <mykilr@gmail.com>
2020-12-02 22:09:28 +0000

    Added simple direction facing + mouse controls

--- PlayerControlledCharacter.gd
+++ PlayerControlledCharacter.gd
@@ -1,15 +1,29 @@
 extends KinematicBody2D
 
-const SPEED = 100
+const SPEED = 200
+onready var sprite := $Sprite
 
 func _physics_process(delta : float):
-	var dir := get_input_dir()
-	dir *= SPEED
+	var mouse = get_global_mouse_position();
+	var dir := _get_input_dir() * SPEED
+	var clicks = _get_input_clicks()
 	
 	move_and_slide(dir)
+	
+	# Rotate the sprite (but not the collision shape)
+	if clicks["a"]:
+		sprite.look_at(mouse)
+	else:
+		sprite.look_at(position + dir)
 
-func get_input_dir() -> Vector2:
+func _get_input_dir() -> Vector2:
 	var dir := Vector2()
 	dir.y = (int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up")))
 	dir.x = (int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")))
 	return dir
+
+func _get_input_clicks() -> Dictionary:
+	return {
+		"a" : Input.is_action_pressed("click_a"),
+		"b" : Input.is_action_pressed("click_b"),
+	}
