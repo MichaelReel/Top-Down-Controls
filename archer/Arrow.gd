f9cbedae1c4ef8dad922a1dea33de1231e676aee
Michael Reel <mykilr@gmail.com>
2020-12-03 21:32:52 +0000

    Moved some stuff, updated animation.

--- archer/Arrow.gd
+++ archer/Arrow.gd
@@ -1,6 +1,12 @@
 extends Area2D
 
 const SPEED = 400
+const LIFE_TIME = 3.0
+var life := LIFE_TIME
 
 func _physics_process(delta):
 	position += transform.x * SPEED * delta
+	
+	life -= delta
+	if life < 0.0:
+		self.queue_free()
--- archer/PlayerControlledCharacter.gd
+++ archer/PlayerControlledCharacter.gd
@@ -3,8 +3,15 @@ extends KinematicBody2D
 export (PackedScene) var Arrow
 
 const SPEED = 200
-onready var sprite := $Sprite
+onready var sprite := $AnimatedSprite
+onready var anim := $AnimatedSprite/AnimationPlayer
+
+var bow_draw := false
 var bow_taut := false
+var bow_loosed := false
+
+func _ready():
+	anim.connect("animation_finished", self, "_anim_done")
 
 func _physics_process(delta : float):
 	var mouse = get_global_mouse_position();
@@ -12,16 +19,29 @@ func _physics_process(delta : float):
 	var clicks = _get_input_clicks()
 	
 	move_and_slide(dir)
+	sprite.look_at(mouse)
 	
 	# Rotate the sprite (but not the collision shape)
 	if clicks["a"]:
-		sprite.look_at(mouse)
+		if bow_draw == false and bow_taut == false:
+			bow_draw = true
+			anim.play("draw")
+	elif bow_draw:
+		anim.play("look")
+		bow_draw = false
+	elif bow_taut:
+		_fire_arrow()
+		anim.play("loose")
+		bow_taut = false
+		bow_loosed = true
+
+func _anim_done(anim_name : String):
+	print("Animation done " + str(bow_draw) + ", " + str(bow_taut))
+	if anim_name == "draw":
 		bow_taut = true
-	else:
-		if bow_taut:
-			_fire_arrow()
-			bow_taut = false
-		sprite.look_at(position + dir)
+		bow_draw = false
+	elif anim_name == "loose":
+		bow_loosed = false
 
 func _get_input_dir() -> Vector2:
 	var dir := Vector2()
