40c09d3e02b629875bb46ffe70f310dd4e8fbb18
Michael Reel <mykilr@gmail.com>
2020-12-06 16:53:44 +0000

    Filled out gateway connection from Game Development Center's youtube video.

---
+++ scenes/singletons/Gateway.gd
@@ -0,0 +1,68 @@
+extends Node
+
+signal login_reset()
+signal login_complete()
+
+var network : NetworkedMultiplayerENet
+var gateway_api : MultiplayerAPI
+var ip = "localhost"
+var port = 1981
+
+var username : String
+var password : String
+
+func _ready():
+	print("READY BEING CALLED!")
+	# TEST: Until login box added:
+	connect_to_server("munch", "munch")
+	# /TEST
+
+func _process(delta):
+	if get_custom_multiplayer() == null:
+		return
+	if not custom_multiplayer.has_network_peer():
+		return
+	custom_multiplayer.poll()
+
+func connect_to_server(_username : String, _password : String):
+	network = NetworkedMultiplayerENet.new()
+	gateway_api = MultiplayerAPI.new()
+	username = _username
+	password = _password
+	print("Attempting to create connection to gateway server")
+	network.create_client(ip, port)
+	set_custom_multiplayer(gateway_api)
+	custom_multiplayer.set_root_node(self)
+	custom_multiplayer.set_network_peer(network)
+	
+	network.connect("connection_failed", self, "_on_connection_failed")
+	network.connect("connection_succeeded", self, "_on_connection_succeeded")
+
+func _on_connection_failed():
+	print("Failed to connect to gateway server")
+	# re-enable login button
+	emit_signal("login_reset")
+
+func _on_connection_succeeded():
+	print("Successfully connected to gateway server")
+	request_login()
+
+func request_login():
+	rpc_id(1, "login_request", username, password)
+	username = ""
+	password = ""
+
+remote func return_login_request(results):
+	print("login results received")
+	if results == true:
+		Server.connect_to_server()
+		# queue free the login screen
+		emit_signal("login_reset")
+	else:
+		print("Authentication failed. Please provide correct username and password")
+		# re-enable login button
+	emit_signal("login_complete")
+	
+	network.disconnect("connection_failed", self, "_on_connection_failed")
+	network.disconnect("connection_succeeded", self, "_on_connection_succeeded")
+		
--- scenes/singletons/Server.gd
+++ scenes/singletons/Server.gd
@@ -1,13 +1,11 @@
 extends Node
 
-var network = NetworkedMultiplayerENet.new()
-var ip = "localhost"
-var port = 1980
+var network := NetworkedMultiplayerENet.new()
+var ip := "localhost"
+var port := 1980
 
-var request_queue = {}
-
-func _ready():
-	connect_to_server()
+var connected := false
+var request_queue := {}
 
 func connect_to_server():
 	network.create_client(ip, port)
@@ -17,14 +15,22 @@ func connect_to_server():
 	
 	network.connect("connection_failed", self, "_on_connection_failed")
 	network.connect("connection_succeeded", self, "_on_connection_succeeded")
+	network.connect("server_disconnected", self, "_on_server_disconnected")
 
 func _on_connection_failed():
 	print("Failed to connect")
 
 func _on_connection_succeeded():
 	print("Successfully connected")
+	connected = true
+
+func _on_server_disconnected():
+	print("Server disconnected")
+	connected = false
 
 func fetch_skill_data(key_path : String, target: Object, ret_func : String):
+	if not connected:
+		return
 	var inst_id : String = str(target.get_instance_id()) + ":" + key_path
 	print("requesting " + str(inst_id))
 	request_queue[inst_id] = {"target" : target, "method" : ret_func}
